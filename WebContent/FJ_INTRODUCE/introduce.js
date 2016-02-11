337c9e3a5063dda6e53bd3f16807799f3b00de8a
credsuk <credsuk@gmail.com>
2016-02-11 12:27:09 +0900

    자기소개서 폼 기능 완성
    Signed-off-by: credsuk <credsuk@gmail.com>

--- WebContent/FJ_INTRODUCE/introduce.js
+++ WebContent/FJ_INTRODUCE/introduce.js
@@ -3,81 +3,85 @@
  */
 // 제목 변경 버트는 누르면 실행되는 메소드
 var cnt = 0;
-function sub_name_modify(id){
-	alert( id );
-	
-	
-	
-	/*
-	// id가 sub_name 이라는 div 사라지게함 
-	$("div[id=sub_name]").css("display", "none");
+function sub_name_modify(count){	
+	// id가 sub_name 이라는 div 사라지게함
+	var sub_name = 'div[id=sub_name'+ count +']';
+	$(sub_name).css("display", "none");
 	// id가 sub_change 이라는 div 나타나게 함
-	$("div[id=sub_change]").css("display", "block");
-	// sub_name_1 div에있는 텍스트 값 인풋폼에 등록
-	$('input[name=sub_change_name]').val( $("div[id=sub_name_1]").text() );
-	*/
+	var sub_change = 'div[id=sub_change'+ count +']';
+	$(sub_change).css("display", "block");
+	// sub_name_ div에있는 텍스트 값 인풋폼에 등록
+	var sub_change_name = 'input[name=sub_change_name'+ count +']';
+	var sub_name_ = 'div[id=sub_name_'+ count +']'
+	$( sub_change_name ).val( $( sub_name_ ).text() );	
 }
+
 // 확인 버튼을 누르면 실행되는 메소드
-function sub_name_ok(){
+function sub_name_ok(count){
+	var sub_name = 'div[id=sub_name'+ count +']';
+	var sub_change = 'div[id=sub_change'+ count +']';
+	var sub_change_name = 'input[name=sub_change_name'+ count +']';
+	var sub_name_ = 'div[id=sub_name_'+ count +']'
 	// div를 받아옴
 	// innerHTML을 이용해 인풋폼에 있는 내용으로 변경
-	$('div[id=sub_name_1]').text( $("input[name=sub_change_name").val() );
+	$(sub_name_).text( $(sub_change_name ).val() );
 	// 인풋폼 초기화
-	$("input[name=sub_change_name").val("");
+	$(sub_change_name).val("");
 	// 위에설명 참고
-	$("div[id=sub_name]").css("display", "block");
-	$("div[id=sub_change]").css("display", "none");
+	$(sub_name).css("display", "block");
+	$(sub_change).css("display", "none");
 }	
 // 취소 버튼을 누르면 실행되는 메소드
-function sub_name_cancel(){
+function sub_name_cancel(count){
+	var sub_name = 'div[id=sub_name'+ count +']';
+	var sub_change = 'div[id=sub_change'+ count +']';
 	//위에 설명 참고
-	$("div[id=sub_name]").css("display", "block");
-	$("div[id=sub_change]").css("display", "none");
+	$(sub_name).css("display", "block");
+	$(sub_change).css("display", "none");
 }
 
 /**
  * 	자기소개서 추가 삭제하기
  */
-// 자기소개서 추가 메소드
+// 자기소개서 페이지 추가 메소드
 function add_introduce(){
-    var standardLength = $('.standard').length;
-    var standardDiv = $('.standard').first().clone().attr('id', function(){ return 'standardID' + cnt;} );
-    var subject = ['자기소개서', '성장과정', '성격', '장단점', '지원동기'];
-    
-    
-    /*
-     * 현재 div에 추가되는거 까지는 됨
-     * 아이디 변화 줬음
-     * 문제는 안에있는 폼들의 이름을 바꿔줘야함
-     * 그래서... 위에 이름 바꾸는 메소드까지 다 바뀜
-     * 천천히 고민합시다~
-     * 
-    if ( standardLength < 10 ) {
-    	for(var i=0; i<standardLength; i++){
-    		if( i == standardLength ){
-    			$(standardDiv).select('sub_name_1').innerHTML = subject[i];
-    			('.standard').appendTo(standardDiv);
-    		}
-    	}
-    }else{
+    var subject = ['자기소개서', '가족소개', '성장과정', '학창시절', '교외활동', '전공선택 이유와 적성', '성격의 장단점', '생활신조', '지원동기', '입사 후 포부'];
+    var standardID = 'div[id=standardID'+ cnt +']'
+    cnt++;
+    if( cnt < 10 ){
+	    var html =	'<div class="standard" id="standardID'+ cnt +'">'
+	    		+	'<div id="sub_name'+ cnt +'"  class="subject" style="display:;">'
+	    		+	'<div id="sub_name_'+ cnt +'" class="subject" >'+ subject[cnt]+'</div>'
+	    		+	'<div class="subject"><input type="button" value="변경" name="modify" onclick="sub_name_modify('+cnt+')"></div>'
+	    		+	'</div>'
+	    		+	'<div id="sub_change'+ cnt +'" class="subject" style="display:none;">'
+	    		+	'<input type="text" name="sub_change_name'+ cnt +'" style="background: #f2f7ff; border: 2px;" autofocus="autofocus" > '
+	    		+	'<input type="button" value="확인" onclick="sub_name_ok('+cnt+')">'
+	    		+	'<input type="button" value="취소" onclick="sub_name_cancel('+cnt+')">'
+	    		+	'</div>'
+	    		+	'<div> <textarea name="contents'+cnt+'" rows="17" cols="100%"></textarea> </div>'
+	    		+	'<div>' 
+	    		+	'<input type="button" value="추가" onclick="add_introduce()">'
+	    		+	'<input type="button" value="삭제" onclick="del_introduce('+cnt+')">'
+	    		+	'</div>'
+	    		+	'</div>';
+	    $(standardID).append(html);
+    } else {
     	alert('작성 가능한 자소서항목은 최대 10개입니다.');
-    }
-   
-    
-    standardDiv.attr(
-    	{
-    		'id' : function(index){ return standardID + index; }
-    	}		
-    );
-    */
-    //standardDiv.attr('id', function(index){ return standardID + index; });
-    //$(standardDiv).find('button[name=modify]').attr('onclick', function(){ return 'sub_name_modify(standardID' + cnt + ')'} );
-    alert( $(standardDiv).find('button[name=modify]').attr('name') );
-	$(standardDiv).appendTo('div[id=introduces]');
-    cnt++; 
-    
-    
-    
+    }  
+}
+
+// 자기소개 페이지 삭제 메소드
+function del_introduce(count){
+	if ( cnt > 0 ) {
+		var delDiv = 'div[id=standardID'+ count +']'
+		if (confirm("삭제하시겠습니까?")) {
+			$(delDiv).remove();
+			cnt--;
+		}
+	} else {
+		alert('한 개 이상의 자소서가 필요합니다.');
+	}
 }
 
 
@@ -97,133 +101,11 @@ $("#tooltip").tooltipsy({
 });
 
 
+/*
 
+http://jquerytools.github.io/demos/tooltip/form.html
 
+http://annotations.tistory.com/66
 
+ */
 
-
-
-
-
-
-
-/**
- * Resume Introduce JavaScript
- * 
- * @requires prototype-1.6.1.js, scriptaculous.js, slider.js
- * @version $Id$ 
- * @copyright (c) 2008-2011 SaraminHR Co., Inc.
- *--------------------------------------------------------------------------
-
-if (!Introduce) { 
-    var Introduce = { Version: '0.9' };
-    Introduce.prototype = {};
-}
-
-//트림함수 정의
-String.prototype.trim = function() {
-    return this.replace(/(^\s*)|(\s*$)/g, "");
-}
-
-//Resume 기본 Class
-Introduce = Class.create();
-Introduce.prototype = {
-    initialize : function () {
-        this.save_status        = '';
-        this.currentPopLayer    = '';
-        this.changeTemplateName = '';
-        this.resIdx             = $F('res_idx');
-        this.currentTemplate    = document.getElementById('current_template').value;
-        this.contentsRsCount    = document.getElementById('contents_rs_count').value;
-        this.writeMode          = document.getElementById('write_mode').value;
-        this.editingFl          = false;
-        this.completeLayerFl    = false;
-        
-        var tooltipOptions = {
-            iframe_id : 'layer_bg_iframe',
-            arrow_id : 'arrow_layer'
-        };
-        
-        this.toolTip = new Saramin.ToolTip(695, tooltipOptions);
-        
-        this.variableAssignment();
-    },
-    
-    // 변수 assign
-    variableAssignment : function () {
-        this.timer                = $('timer');
-        this.autoSave             = $('auto_save');
-        this.saveTime             = $('save_time');
-        this.introTemplate        = document.getElementsByName('intro_template');
-        this.currentMessageLength = document.getElementById('current_message_length');
-        this.currentMessageByte   = document.getElementById('current_message_byte');
-        this.jumpLevel            = $('jump_level');
-        this.contentsForm         = $('contents_form');
-        this.transSeqImage        = $('trans_seq_image');
-        this.lastContents         = $('last_contents');
-        this.lastContentsTitle    = $('last_contents_title');
-    },
-
-/**
- * row 추가
- * 
- * @param object element object {el}
- * @param string add element id {addEl}
- 
-addRow : function (el, addEl) {
-    // 수정 완료 레이어가 열려 있으면 닫는다.
-    this.hideCompleteLayer();
-    
-    var tmpContents    = $('template').down('#' + addEl).innerHTML;
-    var contentsLength = $(el).select('.contents').length;
-    
-    if ( $(el) && contentsLength < 10 ) {
-        new Insertion.Bottom($(el), tmpContents);
-        
-        this.controlAddDelButton(el, 1, '', 'none');
-        
-        this.calcTextLength($(el).down('.introduce-contents', contentsLength));
-        $(el).down('.tmp-contents-title', contentsLength).focus();
-        $(el).down('.tmp-contents-title', contentsLength).select();
-    } else {
-        alert('작성 가능한 자소서항목은 최대 10개입니다.');
-    }
-},
-
-/**
- * row 삭제
- * 
- * @param object element object {el}
- 
-delRow : function (el, eventEl) {
-    // 수정 완료 레이어가 열려 있으면 닫는다.
-    this.hideCompleteLayer();
-    
-    var contentsLength = $(el).select('.contents').length;
-    var contentsEl     = $(eventEl).up('.contents');
-    
-    if ( contentsLength > 2 ) {
-        var cancelDelete = false;
-        
-        if ( contentsEl.down('.introduce-contents') && contentsEl.down('.introduce-contents').value != '' ) {
-            if ( !confirm('항목 삭제시 해당 항목에 작성된 내용이 사라집니다. 정말로 삭제하시겠습니까?') ) {
-                cancelDelete = true;
-            }
-        }
-        
-        if ( cancelDelete == true ) {
-            return;
-        }
-        
-        this.hideLayer();
-        
-        contentsEl.remove();
-        
-        if ( contentsLength == 3 ) {
-            this.controlAddDelButton(el, 1, '', '');
-            this.controlAddDelButton(el, 1, 1, 'none');
-        }
-    }
-},
-}
-*/
--- WebContent/FJ_INTRODUCE/introduceForm.jsp
+++ WebContent/FJ_INTRODUCE/introduceForm.jsp
@@ -10,37 +10,29 @@
 <h2> 자기 소개서 </h2>
 
 <!-- 자기소개서 입력 크기 잡아주기-->
-<div class="introduce">
+<div id="introduce" class="introduce">
 	<!-- 스타일 시스 먹이기 -->
-	<div class="standard" id="standardID">
+	<div class="standard" id="standardID0">
 		<!-- 폼 이름 정해주기 -->
 			<!-- 처음 보여지는 디폴트 값 -->
-			<div id="sub_name"  class="subject">
-				<div id="sub_name_1" class="subject" >자기소개</div>
-				<div class="subject"><input type="button" value="변경" name="modify" onclick="sub_name_modify('standardID')"></div>
+			<div id="sub_name0"  class="subject"  style="display:block;">
+				<div id="sub_name_0" class="subject" >자기소개</div>
+				<div class="subject"><input type="button" value="변경" name="modify" onclick="sub_name_modify('0')"></div>
 			</div>	
 			<!-- 변경을 누르면 나타나는 입력 폼 -->
-			<div id="sub_change" class="subject">
-				<input type="text" id="change_name"  name="sub_change_name" autofocus="autofocus" 
-						style="background: #f2f7ff; border: 2px;"
-						title="Must be at least 8 characters.">
-				<input type="button" value="확인" onclick="sub_name_ok()">
-				<input type="button" value="취소" onclick="sub_name_cancel()">
+			<div id="sub_change0" class="subject" style="display:none;">
+				<input type="text" name="sub_change_name0" autofocus="autofocus" 
+						style="background: #f2f7ff; border: 2px;" >
+				<input type="button" value="확인" onclick="sub_name_ok('0')">
+				<input type="button" value="취소" onclick="sub_name_cancel('0')">
 			</div>
 		<!-- 내용 입력하는 곳 -->
-		<div> <textarea name="contents[]" rows="17" cols="100%"></textarea> </div>
-		<div> <input type="button" value="추가" onclick="add_introduce()"> </div>
+		<div> <textarea name="contents0" rows="17" cols="100%"></textarea> </div>
+		<div> 
+			<input type="button" value="추가" onclick="add_introduce()">
+			<input type="button" value="삭제" onclick="del_introduce('0')">
+		</div>
 	</div>
-	<div id="introduces"></div>
+	<div id="introduces"><input type="button" value="저장" onclick="introducePro.do"></div>
 </div>
 
-
-http://jquerytools.github.io/demos/tooltip/form.html
-
-http://annotations.tistory.com/66
-
-번호를 매겨서
-1번이면 자기소개서
-2번이면 성장과정
-3번이면 등등등....
-이렇게 기본적으로 써지게 하자
